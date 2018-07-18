1f1be49e06d1fa291770dfade454c1237fc5271a
zhh1997zhh <hzhang21@cmc.edu>
2018-07-18 16:44:42 -0700

    wed

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Login from './Login.js';
 import Register from './Register.js';
 import Portal from './Portal.js';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 import createStyles from 'draft-js-custom-styles'
+import io from 'socket.io-client';
+
+let socket = io('http://localhost:1337');
+
 
 const customStyleMap = {
   remoteCursor: {
@@ -42,9 +46,14 @@ export default class App extends React.Component {
     this.state = {
       editorState: EditorState.createEmpty(),
       currentPage: 'Login',
-      userId: ''
+      userId: '',
+      docId: '',
     }
-    this.onChange = (editorState) => this.setState({editorState})
+    this.onChange = (editorState) => {
+      this.setState({editorState})
+      socket.emit('contentState', convertToRaw(editorState.getCurrentContent()));
+      // socket.emit('selectionState', convertToRaw(editorState.getSelection()));
+    }
     this.onToggleStyle = (style) => (e) => {
       const toggleFn = isBlockStyle(style) ? RichUtils.toggleBlockType : RichUtils.toggleInlineStyle
       this.onChange(toggleFn(this.state.editorState, style))
@@ -78,25 +87,91 @@ export default class App extends React.Component {
     }
 
     this.portal = () => {
+      socket.emit('closeDoc');
       this.setState({
         currentPage: "Portal"
       })
     }
 
+    this.logout = () => {
+      localStorage.setItem('login', JSON.stringify(null));
+      this.setState({
+        currentPage: "Login"
+      })
+    }
+
+
     this.userId = (id) => {
       this.setState({userId: id})
       console.log(this.state.userId)
     }
+
+    this.docId = (id) => {
+      this.setState({docId: id})
+      console.log(this.state.docId)
+    }
+
+
+    this.save = () => {
+      fetch('http://localhost:1337/save', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          docId: this.state.docId,
+          content: {
+            content: this.state.editorState,
+            user: this.state.userId
+          },
+          lastEditTime: new Date()
+        })
+      }).then(res => res.json())
+      .then(
+        (resp) => console.log('saved', resp)
+      )
+      .catch(err => console.log('error', err))
+    }
+
+    socket.on('contentState', (cs) => {
+      console.log("Received contentState:", cs);
+      const editorState = EditorState.createWithContent(convertFromRaw(cs));
+      this.setState({
+        editorState: editorState
+      })
+    })
+    //
+    // socket.on('selectionState', (ss) => {
+    //   console.log("Received selectionState:", ss);
+    //   const editorState = EditorState.acceptSelection(editorState, convertFromRaw(ss));
+    //
+    //   this.setState({
+    //     editorState: editorState
+    //   })
+    // })
   }
 
     render() {
     if (this.state.currentPage === "Editor") {
       return (<div>
+        <div>
         <h2 className="title">Document Editor</h2>
+        </div>
+        <div>
+          Document ID: {this.state.docId}
+        </div>
+        <div className="save">
+          <button onClick={() => this.save()}>Save Changes</button>
+        </div>
+        <div>
+          <button onClick={() => this.portal()}>Quit</button>
+        </div>
+        <div className="toolbar">
         {FORMAT_BAR.map(({style, label}) => <button onClick={this.onToggleStyle(style)}>{label}</button>)}
         {['8px','12px','24px'].map(size => <button onClick={this.onSetStyle('fontSize', size)}>{size}</button>)}
         {['red','blue'].map(color => <button onClick={this.onSetStyle('color', color)}>{color}</button>)}
         <button onClick={this.mark}>mark</button>
+        </div>
         <div className="draft-editor-container">
           <Editor
             editorState={this.state.editorState}
@@ -104,6 +179,7 @@ export default class App extends React.Component {
             customStyleFn={customStyleFn}
             blockStyleFn={getBlockStyle}
             onChange={this.onChange}
+            socket={socket}
           />
         </div>
       </div>)
@@ -125,13 +201,17 @@ export default class App extends React.Component {
                        />
     } else if (this.state.currentPage === "Portal") {
       return <Portal username={this.state.username}
+                         documentId={this.state.docId}
+                         editorState={this.state.editorState}
                          currentPage={this.state.currentPage}
-                         userId={this.state.userId}
+                         docId={(id) => this.docId(id)}
                          register={() => this.register()}
                          login={() => this.login()}
                          portal={() => this.portal()}
                          editor={() => this.editor()}
+                         logout={() => this.logout()}
+                         socket={socket}
                        />
     }
-}
+  }
 }
